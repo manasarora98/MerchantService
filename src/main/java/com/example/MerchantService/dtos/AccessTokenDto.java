d84040aadbe52390813d5d87f83d7d1171d856f1
ishit22994 <ishitshah94@gmail.com>
2020-01-25 18:52:17 +0530

    Second Commit

---
+++ src/main/java/com/example/MerchantService/dtos/AccessTokenDto.java
@@ -0,0 +1,12 @@
+package com.example.MerchantService.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AccessTokenDto {
+    int userId;
+    String accessToken;
+
+}
